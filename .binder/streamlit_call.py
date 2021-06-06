ea347078b90d5f24dd7f7142411dd6e45626409b
eotp <krois@zedat.fu-berlin.de>
2021-06-06 03:42:03 +0200

    add app

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -1,5 +1,13 @@
 from subprocess import Popen
 
+
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    Popen(["streamlit", "hello", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(
+        [
+            "streamlit",
+            "06_dashboards_are_fun/test.py",
+            "--browser.serverAddress=0.0.0.0",
+            "--server.enableCORS=False",
+        ]
+    )
