393281d0cffdb7121117539d62f70761e11cf095
eotp <krois@zedat.fu-berlin.de>
2021-10-20 21:11:47 +0200

    fix beiersdorf csv challenge

--- 02_excel_is_dead_long_live_pandas/src/_solutions/transform_data.py
+++ 02_excel_is_dead_long_live_pandas/src/_solutions/transform_data.py
@@ -1,8 +1,8 @@
 def transform_data(df):
     """Function to transform the Beiersdorf fake dataset"""
     df = df.copy()
-    df.Date = pd.to_datetime(df.Date) 
-    df = df.set_index('Date')
-    name = df.Name.unique()
-    df[name] = df['Success']
-    return df[name].groupby(pd.Grouper(freq='1D')).sum().astype(float)
+    df.Date = pd.to_datetime(df.Date)
+    df = df.set_index("Date")
+    name = df.Name.unique()[0]
+    df[name] = df["Success"]
+    return df[name].groupby(pd.Grouper(freq="1D")).sum().astype(float)
